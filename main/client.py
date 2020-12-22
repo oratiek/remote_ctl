8e248b23b7040f47faf7385827e3b7373b44c0d2
oratiek <oratiek522@gmail.com>
2020-12-22 22:42:04 +0900

    update

--- main/client.py
+++ main/client.py
@@ -2,9 +2,9 @@ import socket
 import os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost",5000))
+s.connect(("192.168.10.124",5000))
 #command = "scp main.py localhost:/Users/keitaro/Desktop/projects4/remote_ctl/"
-command = "ls"
+command = "ls /"
 s.send(command.encode())
 output = s.recv(1024)
 print(output.decode())
