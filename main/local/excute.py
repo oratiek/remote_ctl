34a0342c36f963a7c3f455022eeb54c148a8b0b5
oratiek <oratiek522@gmail.com>
2020-12-24 22:34:12 +0900

    update

--- main/local/excute.py
+++ main/local/excute.py
@@ -4,9 +4,13 @@ import sys
 
 def excute(command):
     # return bytes data 
-    res = subprocess.run([command],check=True, shell=True, stdout=subprocess.PIPE)
+    try:
+        res = subprocess.run([command],check=True, shell=True, stdout=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        return "Error".encode()
     res = res.stdout
     return res
 
 if __name__ == "__main__":
-    excute('ls')
+    res = excute('ls /Application')
+    print(res.decode())
