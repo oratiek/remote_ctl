02e485c05b2dff896958efdff680732ceb36dd98
oratiek <oratiek522@gmail.com>
2020-12-24 23:17:29 +0900

    update

--- main/global/client.py
+++ main/global/client.py
@@ -1,10 +1,14 @@
 import socket
 import os
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.10.124",5000))
-#command = "scp main.py localhost:/Users/keitaro/Desktop/projects4/remote_ctl/"
-command = "ls /"
-s.send(command.encode())
-output = s.recv(1024)
-print(output.decode())
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("192.168.10.105",5000))
+    command = input("command;")
+    if command.lower() == "exit":
+        print("client session closed")
+        break
+    else:
+        s.send(command.encode())
+        output = s.recv(1024)
+        print(output.decode())
